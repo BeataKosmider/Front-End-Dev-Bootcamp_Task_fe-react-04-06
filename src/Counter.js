8ad954b7da106ca5ccab0c2c348871f05c059934
Krzysztof Mendrek <kmendrek94@gmail.com>
2022-12-22 12:34:37 +0100

    Task update

--- src/Counter.js
+++ src/Counter.js
@@ -1,27 +1,5 @@
 import React from 'react';
 
-class Counter extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      counter: 5
-    }
-
-    setInterval(() => {
-      let { counter } = this.state;
-      counter--;
-      this.setState({counter})
-    }, 1000)
-  }
-
-  render() {
-    if (this.state.counter > 0) {
-      return <h1>{this.state.counter}</h1>
-    } else {
-      return <h1>Boom!</h1>
-    }
-  }
-}
+class Counter extends React.Component {}
 
 export default Counter;
--- src/Counter.test.js
+++ src/Counter.test.js
@@ -5,18 +5,18 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('List tests', () => {
+describe('Counter tests', () => {
   let wrapper;
 
   it('Poprawnie wyświetla licznik w H1', () => {
     wrapper = mount(<Counter />);
-    expect(wrapper.find('h1').text()).toBe("5")
+    expect(wrapper.find('h1').text()).toBe('5');
   });
 
   it('Poprawnie wyświetla Boom! w H1', () => {
     wrapper = mount(<Counter />);
     setTimeout(() => {
-      expect(wrapper.find('h1').text()).toBe("Boom!");
-    }, 5000)
+      expect(wrapper.find('h1').text()).toBe('Boom!');
+    }, 5000);
   });
 });
