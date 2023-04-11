fa35d46a33db28dc10380b4c48a39eb051d64b58
Beata Kosmider <kosmibe@gmail.com>
2023-04-11 12:46:06 +0200

    done

--- src/Counter.js
+++ src/Counter.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
 class Counter extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      counter: 5
-    }
+      counter: 5,
+    };
 
     setInterval(() => {
       let { counter } = this.state;
       counter--;
-      this.setState({counter})
-    }, 1000)
+      this.setState({ counter });
+    }, 1000);
   }
 
   render() {
     if (this.state.counter > 0) {
-      return <h1>{this.state.counter}</h1>
+      return <h1>{this.state.counter}</h1>;
     } else {
-      return <h1>Boom!</h1>
+      return <h1>Boom!</h1>;
     }
   }
 }
